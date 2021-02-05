0239379065a6792e55774be5d89702d3bfb276d5
Wilson Ludeña <70303021+saul-donas17@users.noreply.github.com>
2021-02-05 13:44:31 -0500

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,5 @@
-
-    fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/302127?note=5&request_method=POST')
+    fetch('https://api.deezer.com/user/2529/playlists')
     .then(res => res.text())
     .then(data =>{
-            console.log(data.data)
+            console.log(data)
     })
-function cancionAnterior(){}
-function play(){}
-function pausa(){}
-function cancionSiguiente(){}
-
---
+++ assets/js/topNav.js
@@ -0,0 +1,7 @@
+function abrirNav(){
+    document.getElementById("nav").style.display="block";
+}
+function cerrarNav(){
+    document.getElementById("nav").style.display="none";
+
+}

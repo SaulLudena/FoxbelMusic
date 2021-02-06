2eccbbfa13905932f3419d7cb849b4c6c21fd01d
Wilson Ludeña <70303021+saul-donas17@users.noreply.github.com>
2021-02-05 19:55:37 -0500

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-       /*
+    /*
     -Antes de empezar, en la 'urlDeezer' del album de deezer tuve que anteponerle
     una url de heroku para evitar el problema 'access-control-allow-origin' (CORS)
     aún no comprendo al 100% el porqué del problema, pero al menos se solucionó.
@@ -61,7 +61,7 @@
     function buscarAlbum(){
         var albumArtista = document.getElementById("albumArtista").value
         console.log(albumArtista)
-        var urlDeezerAlbumPersonalizado = 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/:albumArtista'
+        var urlDeezerAlbumPersonalizado = 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/:albumArtista?albumArtista'
         
         fetch(urlDeezerAlbumPersonalizado)
         .then(res => res.json(),e =>{
